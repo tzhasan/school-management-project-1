6c7f4b66711b7a9f46433c3cfee94947de98b63d
tzhasansohan <sohantajbiul210@gmail.com>
2023-09-22 23:58:33 +0400

    tz to dev

--- src/Layoutes/HomeLayoute.jsx
+++ src/Layoutes/HomeLayoute.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from '../Pages/Home-Page/Home';
+import Banner2 from '../Pages/Home-Page/Banner2';
 
 const HomeLayoute = () => {
   return (
     <div>
       <Home></Home>
+      <Banner2/>
     </div>
   );
 };
