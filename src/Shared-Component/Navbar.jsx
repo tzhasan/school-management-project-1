216dd24fb42a77370a6957059c3aa4314f83730a
mdsumonali372 <mdsumonali026@gmail.com>
2023-09-25 19:08:17 +0600

    navbar some menu add and design add

--- src/Shared-Component/Navbar.jsx
+++ src/Shared-Component/Navbar.jsx
@@ -1,30 +1,67 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+// navlinks ke map kore Navbar e implement korte hobe !!!
+const navLinks = [
+  {
+    name: "Home",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "About Us",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "Teacher",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "Student",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "Results",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "Photo",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "Video",
+    path: "/",
+    logo: "",
+  },
+  {
+    name: "Register",
+    path: "/register",
+    logo: "",
+  },
+  {
+    name: "Login",
+    path: "/login",
+    logo: "",
+  },
+];
 
 const Navbar = () => {
-  // navlinks ke map kore Navbar e implement korte hobe !!!
-  const navlinks = [
-    {
-      name: "Home",
-      links: "/",
-      logo: "",
-    },
-    {
-      name: "About Us",
-      links: "/",
-      logo: "",
-    },
-    {
-      name: "Register",
-      links: "/register",
-      logo: "",
-    },
-    {
-      name: "Login",
-      links: "/login",
-      logo: "",
-    },
-  ];
-  return <div>Navbar</div>;
+  return (
+    <div className="container mx-auto bg-[#051939] h-14 flex items-center">
+      <ul className="flex items-center text-white text-[18px]">
+        {navLinks.map((link, index) => (
+          <li className="ml-4" key={index}>
+            <NavLink to={link.path}>{link.name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
